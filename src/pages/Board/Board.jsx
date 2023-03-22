043aad2ae07a738c6c4c84c409b984624385fd1f
ChanwooPark <chanw9503@gmail.com>
2023-03-23 03:05:02 +0900

    [STYLE]
    Board style separation

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -14,6 +14,16 @@ import LabelTextArea from '../../components/LabelTextarea/LabelTextArea';
 import useInput from '../../hooks/useInput';
 import { __boardModify, __boardWriting } from '../../redux/modules/boardSlice';
 import { useNavigate } from 'react-router-dom';
+import {
+  StyledButtonBox,
+  StyledWrap,
+  StyledBoardBlock,
+  StyledPlus,
+  StyledImageBlock,
+  StyledBodyForm,
+  StyledTitleBox,
+  StyledLabel,
+} from './styles';
 
 function Board() {
   const [title, setTitle] = useInput();
@@ -65,57 +75,5 @@ function Board() {
     </StyledWrap>
   );
 }
-
-const StyledButtonBox = styled.div`
-  display: flex;
-  justify-content: right;
-`;
-
-const StyledWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 90vh;
-`;
-
-const StyledBoardBlock = styled.div`
-  width: 800px;
-  height: 1100px;
-  border: 1px solid green;
-`;
-
-const StyledPlus = styled.span`
-  font-size: 110px;
-  font-weight: 700;
-`;
-
-const StyledImageBlock = styled.div`
-  width: 100%;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid black;
-`;
-
-const StyledBodyForm = styled.form`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 30px;
-`;
-
-const StyledTitleBox = styled.div`
-  width: 100%;
-  height: 2.5rem;
-  font-size: 1.5rem;
-  display: flex;
-  align-items: center;
-`;
-
-const StyledLabel = styled.label`
-  font-size: 30px;
-  font-weight: 700;
-`;
 
 export default Board;
