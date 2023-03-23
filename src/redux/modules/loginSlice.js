b4672861f2344b80631b1927a11482ab69eaf71b
yeooong <yeong.kim9127@gmail.com>
2023-03-23 17:23:14 +0900

    [FIX]
    사라진 코드 복원

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -39,7 +39,11 @@ export const __login = createAsyncThunk('Login', async (payload, thunkAPI) => {
   try {
     const response = await api.post('/api/auth/login', payload);
     setCookie('token', response.headers.authorization);
-    return thunkAPI.fulfillWithValue(response.data);
+    return thunkAPI.fulfillWithValue(response.data); 
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
 
 
 export const __logout = createAsyncThunk('logout', async (payload, thunkAPI) => {
