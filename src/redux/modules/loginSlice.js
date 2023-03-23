be222a3a23b9750cf40d4809fe26254648c4c9db
ChanwooPark <chanw9503@gmail.com>
2023-03-23 17:22:26 +0900

    [FIX] bug fix

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -2,17 +2,17 @@
  * 작성자: 김은영, 박찬우
  * 목적: login시 필요한 slice생성
  * 작성 날짜: 2023-03-17
- * 
+ *
  * 작성자: 김은영
  * 목적: login시 필요한 Slice 구축
  * 작성 날짜: 2023-03-18
  *
  * 수정: 500error, payload 에러
- * 
+ *
  * 수정자 : 박찬우
  * 목적 : logout api 제거. (logout 시 클라이언트쪽에서 쿠기 제거만 한다.)
  * 작성 날짜 : 2023.03.21
- * 
+ *
  * 수성자: 김은영
  * 목적: reject 됐을 때 서버에서 받아온 error메세지를 띄우기 위해 수정.
  * 작성 날짜: 2023-03-22
@@ -34,13 +34,15 @@ const initialState = {
   error: null,
 };
 
-
 export const __login = createAsyncThunk('Login', async (payload, thunkAPI) => {
   try {
     const response = await api.post('/api/auth/login', payload);
     setCookie('token', response.headers.authorization);
     return thunkAPI.fulfillWithValue(response.data);
-
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
 
 export const __logout = createAsyncThunk('logout', async (payload, thunkAPI) => {
   try {
@@ -65,7 +67,19 @@ const loginSlice = createSlice({
     [__login.rejected]: (state, action) => {
       state.isLoading = false;
       state.isError = true;
-      alert(action.payload.data.msg)
+      alert(action.payload.data.msg);
+    },
+    [__logout.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__logout.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+    },
+    [__logout.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      alert(action.payload.data.msg);
     },
   },
 });
